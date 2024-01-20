770a8cb17e57a4a7f92ecac07c550fe1413e273b
Youness91-Tech <bayttarbawi.com@gmail.com>
2024-01-19 17:10:38 -0800

    0-create_a_file.pp updated

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,8 +1,8 @@
-# creates a file in /tmp with specific reqs
+# Creates a file in /tmp with specific reqs
 file { '/tmp/school':
-  content => 'I love Puppet',
   ensure  => file,
   mode    => '0744',
   owner   => 'www-data',
   group   => 'www-data',
+  content => 'I love Puppet',
 }
