6488441d8c768e6a3ce4fab05d60aba29307aa92
Youness91-tech <bayttarbawi.com@gmail.com>
2024-04-11 15:36:41 -0700

    2-recurse.py

---
+++ 0x16-api_advanced/2-recurse.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""
+Queries the Reddit API and returns
+a list containing the titles of all hot articles for a given subreddit.
+"""
+import requests
+
+
+def recurse(subreddit, hot_list=[], after=None):
+    """Returns a list containing the titles of all hot articles for a given"""
+    user_agent = {'User-Agent': 'api_advanced-project'}
+    url = "https://www.reddit.com/r/{}/hot.json".format(subreddit)
+    parameters = {'after': after}
+    results = requests.get(url, params=parameters, headers=user_agent,
+                           allow_redirects=False)
+
+    if results.status_code == 200:
+        after_data = results.json().get("data").get("after")
+        if after_data is not None:
+            after = after_data
+            recurse(subreddit, hot_list)
+        data = results.json().get("data").get("children")
+        for child in data:
+            hot_list.append(child.get("data").get("title"))
+        return hot_list
+    else:
+        return (None)
